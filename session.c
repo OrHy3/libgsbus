488e7bd0792ae35d9be575e114d9d2629619246a
OrHy3 <orhy3@pm.me>
2024-11-24 14:48:07 +0100

    Partial commit for testing purposes

--- session.c
+++ session.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/random.h>
 #include <dbus/dbus.h>
 
@@ -10,6 +11,13 @@
 struct gs_Session {
 	DBusConnection *connection;
 	const char *session_id;
+	char token_string[256];
+};
+
+struct gs_Shortcut {
+	const char *name;
+	const char *description;
+	const char *trigger;
 };
 
 enum gs_ErrorCode {
@@ -23,18 +31,23 @@ enum gs_ErrorCode {
 	MATCH_RULE_ERROR
 };
 
-int gs_open_session(struct gs_Session *session, DBusError *error) {
+int gs_CreateSession(struct gs_Session *session, const char *app_id, DBusError *error) {
 
 	if (error != NULL)
 		dbus_error_init(error);
 
 	session->connection = dbus_bus_get(DBUS_BUS_SESSION, error);
 	if (error != NULL && dbus_error_is_set(error)) {
+		dbus_connection_close(session->connection);
+		session->connection = NULL;
 		return CONNECTION_ERROR;
 	}
 
-	if (session->connection == NULL)
+	if (session->connection == NULL) {
+		dbus_connection_close(session->connection);
+		session->connection = NULL;
 		return BAD_CONNECTION;
+	}
 
 	DBusMessage *message;
 	message = dbus_message_new_method_call(
@@ -43,23 +56,27 @@ int gs_open_session(struct gs_Session *session, DBusError *error) {
 		PORTAL_INTERFACE,
 		"CreateSession"
 	);
-	if (message == NULL)
+	if (message == NULL) {
+		dbus_connection_close(session->connection);
+		session->connection = NULL;
 		return MSG_CREATION_ERROR;
+	}
 
 	unsigned random_id;
 	getrandom(&random_id, sizeof(random_id), GRND_NONBLOCK);
 
-	char token_string[15];
-	snprintf(token_string, sizeof(token_string), "gsbus_%d\0", random_id % 10000);
-	const char *token_string_ptr = token_string;
+	snprintf(session->token_string, sizeof(session->token_string), "gsbus_%d\0", random_id % 10000);
+	const char *token_string_ptr = session->token_string;
 
 	DBusMessageIter args;
+	DBusMessageIter array_iter;
+	DBusMessageIter dict_iter;
+	DBusMessageIter variant_iter;
+
 	dbus_message_iter_init_append(message, &args);
 
-	DBusMessageIter array_iter;
 	dbus_message_iter_open_container(&args, DBUS_TYPE_ARRAY, "{sv}", &array_iter);
 
-	DBusMessageIter dict_iter;
 	dbus_message_iter_open_container(&array_iter, DBUS_TYPE_DICT_ENTRY, NULL, &dict_iter);
 
 	char *dict_keys[2] = {
@@ -69,7 +86,6 @@ int gs_open_session(struct gs_Session *session, DBusError *error) {
 
 	dbus_message_iter_append_basic(&dict_iter, DBUS_TYPE_STRING, &dict_keys[0]);
 
-	DBusMessageIter variant_iter;
 	dbus_message_iter_open_container(&dict_iter, DBUS_TYPE_VARIANT, "s", &variant_iter);
 
 	dbus_message_iter_append_basic(&variant_iter, DBUS_TYPE_STRING, &token_string_ptr);
@@ -83,7 +99,10 @@ int gs_open_session(struct gs_Session *session, DBusError *error) {
 
 	dbus_message_iter_open_container(&dict_iter, DBUS_TYPE_VARIANT, "s", &variant_iter);
 
-	dbus_message_iter_append_basic(&variant_iter, DBUS_TYPE_STRING, &token_string_ptr);
+	if (app_id == NULL)
+		dbus_message_iter_append_basic(&variant_iter, DBUS_TYPE_STRING, &token_string_ptr);
+	else
+		dbus_message_iter_append_basic(&variant_iter, DBUS_TYPE_STRING, &app_id);
 
 	dbus_message_iter_close_container(&dict_iter, &variant_iter);
 	dbus_message_iter_close_container(&array_iter, &dict_iter);
@@ -92,8 +111,269 @@ int gs_open_session(struct gs_Session *session, DBusError *error) {
 	DBusMessage *reply;
 	reply = dbus_connection_send_with_reply_and_block(session->connection, message, DBUS_TIMEOUT_USE_DEFAULT, error);
 	dbus_message_unref(message);
-	if (error != NULL && dbus_error_is_set(error))
+	if (error != NULL && dbus_error_is_set(error)) {
+		if (reply)
+			dbus_message_unref(reply);
+		dbus_connection_unref(session->connection);
+		session->connection = NULL;
 		return REPLY_ERROR;
+	}
+
+	if (reply == NULL) {
+		dbus_connection_unref(session->connection);
+		session->connection = NULL;
+		return BAD_REPLY;
+	}
+
+	dbus_message_iter_init(reply, &args);
+
+	const char *request_id;
+	dbus_message_iter_get_basic(&args, &request_id);
+
+	dbus_message_unref(reply);
+
+	char match_rule[256];
+	snprintf(match_rule, sizeof(match_rule), "type='signal',interface='org.freedesktop.portal.Request',path='%s'", request_id);
+
+	dbus_bus_add_match(session->connection, match_rule, error);
+	if (error != NULL && dbus_error_is_set(error)) {
+		dbus_connection_close(session->connection);
+		session->connection = NULL;
+		return MATCH_RULE_ERROR;
+	}
+
+	dbus_connection_flush(session->connection);
+
+	do {
+		dbus_connection_read_write(session->connection, 0);
+		reply = dbus_connection_pop_message(session->connection);
+	} while (reply == NULL || !dbus_message_is_signal(reply, "org.freedesktop.portal.Request", "Response"));
+
+	dbus_bus_remove_match(session->connection, match_rule, error);
+	if (error != NULL && dbus_error_is_set(error)) {
+		dbus_message_unref(reply);
+		dbus_connection_close(session->connection);
+		session->connection = NULL;
+		return MATCH_RULE_ERROR;
+	}
+
+	dbus_message_iter_init(reply, &args);
+
+	int status;
+	dbus_message_iter_get_basic(&args, &status);
+	if (status != 0) {
+		dbus_message_unref(reply);
+		dbus_connection_close(session->connection);
+		session->connection = NULL;
+		return BAD_SIGNAL;
+	}
+
+	dbus_message_iter_next(&args);
+
+	dbus_message_iter_recurse(&args, &array_iter);
+	dbus_message_iter_recurse(&array_iter, &dict_iter);
+
+	dbus_message_iter_next(&dict_iter);
+
+	dbus_message_iter_recurse(&dict_iter, &variant_iter);
+
+	dbus_message_iter_get_basic(&variant_iter, &session->session_id);
+
+	dbus_message_unref(reply);
+	
+	return 0;
+
+}
+
+void gs_CloseSession(struct gs_Session *session) {
+
+	if (session == NULL)
+		return;
+
+	if (session->connection && dbus_connection_get_is_connected(session->connection)) {
+
+		if (session->connection) {
+
+			DBusMessage *message = dbus_message_new_method_call(
+				PORTAL_TARGET,
+				session->session_id,
+				"org.freedesktop.portal.Session",
+				"Close"
+			);
+
+			dbus_connection_send(session->connection, message, NULL);
+
+		}
+
+		dbus_connection_unref(session->connection);
+
+	}
+
+	session->session_id = NULL;
+
+}
+
+int gs_BindShortcuts(struct gs_Session *session, struct gs_Shortcut *shortcut_list, int num, DBusError *error) {
+
+	if (error != NULL)
+		dbus_error_init(error);
+
+	DBusMessage *message = dbus_message_new_method_call(
+		PORTAL_TARGET,
+		PORTAL_OBJECT,
+		PORTAL_INTERFACE,
+		"BindShortcuts"
+	);
+
+	DBusMessageIter args;
+	DBusMessageIter array_iter;
+	DBusMessageIter struct_iter;
+	DBusMessageIter subarray_iter;
+	DBusMessageIter dict_iter;
+	DBusMessageIter variant_iter;
+
+	dbus_message_iter_init_append(message, &args);
+
+	dbus_message_iter_append_basic(&args, DBUS_TYPE_OBJECT_PATH, &session->session_id);
+
+	dbus_message_iter_open_container(&args, DBUS_TYPE_ARRAY, "(sa{sv})", &array_iter);
+
+	for (int i = 0; i < num; i++) {
+
+		dbus_message_iter_open_container(&array_iter, DBUS_TYPE_STRUCT, NULL, &struct_iter);
+	
+		dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_STRING, &shortcut_list[i].name);
+	
+		dbus_message_iter_open_container(&struct_iter, DBUS_TYPE_ARRAY, "{sv}", &subarray_iter);
+		dbus_message_iter_open_container(&subarray_iter, DBUS_TYPE_DICT_ENTRY, NULL, &dict_iter);
+	
+		const char *description_key = "description";
+		dbus_message_iter_append_basic(&dict_iter, DBUS_TYPE_STRING, &description_key);
+	
+		dbus_message_iter_open_container(&dict_iter, DBUS_TYPE_VARIANT, "s", &variant_iter);
+	
+		dbus_message_iter_append_basic(&variant_iter, DBUS_TYPE_STRING, &shortcut_list[i].description);
+	
+		dbus_message_iter_close_container(&dict_iter, &variant_iter);
+		dbus_message_iter_close_container(&subarray_iter, &dict_iter);
+
+		if (shortcut_list[i].trigger != NULL) {
+		
+			dbus_message_iter_open_container(&subarray_iter, DBUS_TYPE_DICT_ENTRY, NULL, &dict_iter);
+
+			const char *trigger_key = "preferred_trigger";
+			dbus_message_iter_append_basic(&dict_iter, DBUS_TYPE_STRING, &trigger_key);
+
+			dbus_message_iter_open_container(&dict_iter, DBUS_TYPE_VARIANT, "s", &variant_iter);
+
+			dbus_message_iter_append_basic(&variant_iter, DBUS_TYPE_STRING, &shortcut_list[i].trigger);
+	
+			dbus_message_iter_close_container(&dict_iter, &variant_iter);
+			dbus_message_iter_close_container(&subarray_iter, &dict_iter);
+
+		}
+
+		dbus_message_iter_close_container(&struct_iter, &subarray_iter);
+		dbus_message_iter_close_container(&array_iter, &struct_iter);
+
+	}
+
+	dbus_message_iter_close_container(&args, &array_iter);
+
+	const char *parent_window = "";
+	dbus_message_iter_append_basic(&args, DBUS_TYPE_STRING, &parent_window);
+
+	dbus_message_iter_open_container(&args, DBUS_TYPE_ARRAY, "{sv}", &array_iter);
+
+	dbus_message_iter_open_container(&array_iter, DBUS_TYPE_DICT_ENTRY, NULL, &dict_iter);
+
+	unsigned random_id;
+	getrandom(&random_id, sizeof(random_id), GRND_NONBLOCK);
+
+	const char *dict_key = "handle_token";
+	char token_string[256];
+	do {
+		snprintf(token_string, sizeof(token_string), "gsbus_%d\0", random_id % 10000);
+	} while (strcmp(session->token_string, token_string) == 0);
+	const char *token_string_ptr = token_string;
+
+	dbus_message_iter_append_basic(&dict_iter, DBUS_TYPE_STRING, &dict_key);
+
+	dbus_message_iter_open_container(&dict_iter, DBUS_TYPE_VARIANT, "s", &variant_iter);
+
+	dbus_message_iter_append_basic(&variant_iter, DBUS_TYPE_STRING, &token_string_ptr);
+
+	dbus_message_iter_close_container(&dict_iter, &variant_iter);
+	dbus_message_iter_close_container(&array_iter, &dict_iter);
+	dbus_message_iter_close_container(&args, &array_iter);
+
+	DBusMessage *reply = dbus_connection_send_with_reply_and_block(session->connection, message, DBUS_TIMEOUT_USE_DEFAULT, error);
+	dbus_message_unref(message);
+	if (error != NULL && dbus_error_is_set(error)) {
+		if (reply)
+			dbus_message_unref(reply);
+		return REPLY_ERROR;
+	}
+
+	if (reply == NULL)
+		return BAD_REPLY;
+
+	dbus_message_unref(reply);
+
+	return 0;
+
+}
+
+int gs_ListShortcuts(struct gs_Session *session, struct gs_Shortcut **shortcut_list, int *num, DBusError *error) {
+
+	if (error != NULL)
+		dbus_error_init(error);
+
+	DBusMessage *message = dbus_message_new_method_call(
+		PORTAL_INTERFACE,
+		PORTAL_OBJECT,
+		PORTAL_TARGET,
+		"ListShortcuts"
+	);
+
+	DBusMessageIter args;
+	DBusMessageIter array_iter;
+	DBusMessageIter struct_iter;
+	DBusMessageIter subarray_iter;
+	DBusMessageIter dict_iter;
+	DBusMessageIter variant_iter;
+
+	dbus_message_iter_init_append(message, &args);
+
+	dbus_message_iter_append_basic(&args, DBUS_TYPE_OBJECT_PATH, &session->session_id);
+
+	dbus_message_iter_open_container(&args, DBUS_TYPE_ARRAY, "{sv}", &array_iter);
+	dbus_message_iter_open_container(&array_iter, DBUS_TYPE_DICT_ENTRY, NULL, &dict_iter);
+
+	const char *options_key = "handle_token";
+	dbus_message_iter_append_basic(&dict_iter, DBUS_TYPE_STRING, &options_key);
+
+	dbus_message_iter_open_container(&dict_iter, DBUS_TYPE_VARIANT, "s", &variant_iter);
+
+	unsigned random_id;
+	getrandom(&random_id, sizeof(random_id), GRND_NONBLOCK);
+	char token_string[256];
+	snprintf(token_string, sizeof(token_string), "gsbus_%d\0", random_id % 10000);
+	const char *token_string_ptr = token_string;
+
+	dbus_message_iter_append_basic(&variant_iter, DBUS_TYPE_STRING, &token_string_ptr);
+
+	dbus_message_iter_close_container(&dict_iter, &variant_iter);
+	dbus_message_iter_close_container(&array_iter, &dict_iter);
+	dbus_message_iter_close_container(&args, &array_iter);
+
+	DBusMessage *reply = dbus_connection_send_with_reply_and_block(session->connection, message, DBUS_TIMEOUT_USE_DEFAULT, error);
+	dbus_message_unref(message);
+	if (error != NULL && dbus_error_is_set(error)) {
+		if (reply)
+			dbus_message_unref(reply);
+		return REPLY_ERROR;
+	}
 
 	if (reply == NULL)
 		return BAD_REPLY;
@@ -102,8 +382,6 @@ int gs_open_session(struct gs_Session *session, DBusError *error) {
 
 	const char *request_id;
 	dbus_message_iter_get_basic(&args, &request_id);
-
-	puts(request_id);
 
 	dbus_message_unref(reply);
 
@@ -122,37 +400,73 @@ int gs_open_session(struct gs_Session *session, DBusError *error) {
 	} while (reply == NULL || !dbus_message_is_signal(reply, "org.freedesktop.portal.Request", "Response"));
 
 	dbus_bus_remove_match(session->connection, match_rule, error);
-	if (error != NULL && dbus_error_is_set(error))
+	if (error != NULL && dbus_error_is_set(error)) {
+		dbus_message_unref(reply);
 		return MATCH_RULE_ERROR;
+	}
 
 	dbus_message_iter_init(reply, &args);
 
 	int status;
 	dbus_message_iter_get_basic(&args, &status);
-	if (status != 0)
+	if (status != 0) {
+		dbus_message_unref(reply);
 		return BAD_SIGNAL;
+	}
 
 	dbus_message_iter_next(&args);
 
-	dbus_message_iter_recurse(&args, &array_iter);
-	dbus_message_iter_recurse(&array_iter, &dict_iter);
+	DBusMessageIter suparray_iter;
+	DBusMessageIter supdict_iter;
+	DBusMessageIter supvariant_iter;
+	dbus_message_iter_recurse(&args, &suparray_iter);
+	dbus_message_iter_recurse(&suparray_iter, &supdict_iter);
 
-	dbus_message_iter_next(&dict_iter);
+	dbus_message_iter_next(&supdict_iter);
 
-	dbus_message_iter_recurse(&dict_iter, &variant_iter);
+	dbus_message_iter_recurse(&supdict_iter, &supvariant_iter);
+	dbus_message_iter_recurse(&supvariant_iter, &array_iter);
 
-	dbus_message_iter_get_basic(&variant_iter, &session->session_id);
-	printf("%s\n", session->session_id);
-	
 	return 0;
 
 }
 
 int main() {
+
 	struct gs_Session session;
 	DBusError error;
-	printf("%d\n", gs_open_session(&session, &error));
+	printf("%d\n", gs_CreateSession(&session, "GS_TEST", &error));
 
-	if (dbus_error_is_set(&error))
+	if (dbus_error_is_set(&error)) {
 		puts(error.message);
+		dbus_error_free(&error);
+		return 0;
+	}
+
+	puts(session.session_id);
+	// while (1);
+
+	struct gs_Shortcut shortcut;
+	shortcut.name = "BANANA";
+	shortcut.description = "Fill the form here (no scam)";
+	shortcut.trigger = "LOGO+m";
+
+	printf("%d\n", gs_BindShortcuts(&session, &shortcut, 1, &error));
+
+	if (dbus_error_is_set(&error)) {
+		puts(error.message);
+		dbus_error_free(&error);
+		return 0;
+	}
+
+	printf("%d\n", gs_ListShortcuts(&session, NULL, NULL, &error));
+
+	if (dbus_error_is_set(&error)) {
+		puts(error.message);
+		dbus_error_free(&error);
+		return 0;
+	}
+
+	gs_CloseSession(&session);
+
 }
